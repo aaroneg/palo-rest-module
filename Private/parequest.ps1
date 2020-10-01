45b83452edd0a98530a854950a2d9114845d0f61
Aaron Glenn <aaron@aaroneg.com>
2020-10-01 00:08:24 -0500

    verbose output for those following along at home

--- Private/parequest.ps1
+++ Private/parequest.ps1
@@ -7,6 +7,7 @@ function Invoke-PaRequest {
         "X-PAN-KEY" = $paConnection.ApiKey
         ContentType = 'application/json'
     }
+    Write-Verbose "[$($MyInvocation.MyCommand.Name)] Making API call."
     try {
         $result = Invoke-RestMethod @restParams -Headers $headers
     }
@@ -19,5 +20,4 @@ function Invoke-PaRequest {
         }
     }
     $result
-    $restParams
 }

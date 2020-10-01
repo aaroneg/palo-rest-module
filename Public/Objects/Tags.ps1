c8d1431682d33bfd309a459a65602855459053da
Aaron Glenn <aaron@aaroneg.com>
2020-10-01 01:33:24 -0500

    #ocd

--- Public/Objects/Tags.ps1
+++ Public/Objects/Tags.ps1
@@ -1,61 +1,61 @@
 function Get-PATags {
     [CmdletBinding()]
     Param(
-        [Parameter(Mandatory=$False)][object]$paConnection=$Script:paConnection
+        [Parameter(Mandatory = $False)][object]$paConnection = $Script:paConnection
     )
     Write-Verbose "[$($MyInvocation.MyCommand.Name)] This cmdlet will not show tags from the 'Predefined' location, only tags that exist in a vsys."
-    $ObjectAPIURI="$($paConnection.ApiBaseUrl)Objects/Tags?"
-    $Arguments= @{
+    $ObjectAPIURI = "$($paConnection.ApiBaseUrl)Objects/Tags?"
+    $Arguments = @{
         location = 'vsys'
         vsys     = $($paConnection.VSys)
     }
-    $Argumentstring=(New-PaArgumentString $Arguments)
-    $restParams=@{
+    $Argumentstring = (New-PaArgumentString $Arguments)
+    $restParams = @{
         Method               = 'Get'
         Uri                  = "$($ObjectAPIURI)$($ArgumentString)"
         SkipCertificateCheck = $True
     }
     $Result = Invoke-PaRequest $restParams
-    ($result.result).entry
+    ($Result.result).entry
 }
 function Get-PATag {
     [CmdletBinding()]
     Param(
-        [Parameter(Mandatory=$False)][object]$paConnection=$Script:paConnection,
-        [Alias("AddressName")][Parameter(Mandatory=$True,Position=0)][string]$Name
+        [Parameter(Mandatory = $False)][object]$paConnection = $Script:paConnection,
+        [Alias("AddressName")][Parameter(Mandatory = $True, Position = 0)][string]$Name
     )
-    $ObjectAPIURI="$($paConnection.ApiBaseUrl)Objects/Tags?"
-    $Arguments=@{
+    $ObjectAPIURI = "$($paConnection.ApiBaseUrl)Objects/Tags?"
+    $Arguments = @{
         location = 'vsys'
         vsys     = $($paConnection.VSys)
         name     = $Name
     }
-    $Argumentstring=(New-PaArgumentString $Arguments)
+    $Argumentstring = (New-PaArgumentString $Arguments)
     
-    $restParams=@{
+    $restParams = @{
         Method               = 'Get'
         Uri                  = "$($ObjectAPIURI)$($ArgumentString)"
         SkipCertificateCheck = $True
     }
     $Result = Invoke-PaRequest $restParams
-    ($result.result).entry
+    ($Result.result).entry
 }
 
 function New-PATag {
     [CmdletBinding()]
     Param(
-        [Parameter(Mandatory=$False)][object]$paConnection=$Script:paConnection,
-        [Parameter(Mandatory=$True,Position=0)][string]$Name,
-        [Parameter(Mandatory=$false,Position=1)][string]$Comments=''
+        [Parameter(Mandatory = $False)][object]$paConnection = $Script:paConnection,
+        [Parameter(Mandatory = $True, Position = 0)][string]$Name,
+        [Parameter(Mandatory = $false, Position = 1)][string]$Comments = ''
     )
-    $ObjectAPIURI="$($paConnection.ApiBaseUrl)Objects/Tags?"
-    $Arguments= @{
+    $ObjectAPIURI = "$($paConnection.ApiBaseUrl)Objects/Tags?"
+    $Arguments = @{
         location = 'vsys'
         vsys     = $($paConnection.VSys)
         name     = $Name
     }
-    $Argumentstring=(New-PaArgumentString $Arguments)
-    [psobject]$newObject=@{
+    $Argumentstring = (New-PaArgumentString $Arguments)
+    [psobject]$newObject = @{
         entry = @{
             "@name"     = $Name
             "@location" = "vsys"
@@ -63,11 +63,11 @@ function New-PATag {
         }
     }
 
-    $restParams=@{
+    $restParams = @{
         Method               = 'post'
         Uri                  = "$($ObjectAPIURI)$($ArgumentString)"
         SkipCertificateCheck = $True
-        body                 = $newObject|ConvertTo-Json
+        body                 = $newObject | ConvertTo-Json
     }
     "[$($MyInvocation.MyCommand.Name)] Submitting '$Name' to API endpoint."
     $Result = Invoke-PaRequest $restParams

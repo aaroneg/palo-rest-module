686130899855abd11671e59de5cb35c808de53e7
Aaron Glenn <aaron@aaroneg.com>
2020-10-04 16:28:13 -0500

    formatting, testing

--- Public/Objects/AddressGroups.ps1
+++ Public/Objects/AddressGroups.ps1
@@ -1,13 +1,13 @@
-function Get-PAAddressGroup {
+function Get-PAAddressGroups {
     [CmdletBinding()]
     Param(
         [Parameter(Mandatory = $False)][object]$paConnection = $Script:paConnection
     )
     $ObjectAPIURI = "$($paConnection.ApiBaseUrl)Objects/AddressGroups?"
-    $Arguments = @(
-        "location=vsys"
-        "vsys=$($paConnection.VSys)"
-    )
+    $Arguments = @{
+        location = 'vsys'
+        vsys     = $($paConnection.VSys)
+    }
     $Argumentstring = (New-PaArgumentString $Arguments)
     $restParams = @{
         Method               = 'Get'
@@ -25,7 +25,7 @@ function Get-PAAddressGroup {
     )
     $ObjectAPIURI = "$($paConnection.ApiBaseUrl)Objects/AddressGroups?"
     $Arguments = @{
-        location = vsys
+        location = 'vsys'
         vsys     = $($paConnection.VSys)
         name     = $Name
     }
@@ -39,13 +39,14 @@ function Get-PAAddressGroup {
     ($result.result).entry
 }
 
-function New-PAAddressGroup {
+function New-PAAddressStaticGroup {
     [CmdletBinding()]
     Param(
         [Parameter(Mandatory = $False)][object]$paConnection = $Script:paConnection,
         [Parameter(Mandatory = $True, Position = 0)][string]$Name,
-        [Parameter(Mandatory = $True, Position = 1)][string]$ipNetmask,
-        [Parameter(Mandatory = $false, Position = 2)][string]$description = ''
+        [Parameter(Mandatory = $True, Position = 1)][string]$description,
+        [Parameter(Mandatory = $false, Position = 2)][array]$Addresses,
+        [Parameter(Mandatory=$false,Position=3)][array]$Tags
     )
     $ObjectAPIURI = "$($paConnection.ApiBaseUrl)Objects/AddressGroups?"
     $Arguments = @{
@@ -58,16 +59,35 @@ function New-PAAddressGroup {
         entry = @{
             '@name'      = $Name
             '@location'  = "vsys"
-            'ip-netmask' = $ipNetmask
             description  = $description
         }
     }
-
+    if ($Addresses) {
+        $newObject.entry.static=@{member=@()}
+        foreach ($Address in $Addresses) {
+            Write-Verbose "[$($MyInvocation.MyCommand.Name)] Adding address '$Address' => $Name"
+            $newObject.entry.static.member+=[object]$Address
+        }
+    }
+    if ($Tags) {
+        $newObject.entry.tag=@{member=[array]@()}
+        foreach ($Tag in $Tags) {
+            try {$foo=Get-PATag -Name $Tag -ErrorAction Stop}
+            catch {
+                Write-Warning "[$($MyInvocation.MyCommand.Name)] Tag '$Tag' was not found. Adding."
+                try {New-PATag -Name $Tag}
+                catch {throw "[$($MyInvocation.MyCommand.Name)] Unable to process $Name"}
+            }
+            Write-Verbose "[$($MyInvocation.MyCommand.Name)] Adding tag '$tag' => $Name"
+            $newObject.entry.tag.member+= $Tag
+        }
+    }
+    Write-Verbose "[$($MyInvocation.MyCommand.Name)] $($newObject|convertto-json -Depth 50)"
     $restParams = @{
         Method               = 'post'
         Uri                  = "$($ObjectAPIURI)$($ArgumentString)"
         SkipCertificateCheck = $True
-        body                 = $newObject | ConvertTo-Json
+        body                 = $newObject | ConvertTo-Json -Depth 50
     }
 
     $Result = Invoke-PaRequest $restParams -Verbose:$False

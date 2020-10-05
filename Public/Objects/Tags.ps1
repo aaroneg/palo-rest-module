dcc22f849d2281dd9b09f2e4f22aebfc25fd1c91
Aaron Glenn <aaron@aaroneg.com>
2020-10-04 19:46:50 -0500

    Implement set-patag

--- Public/Objects/Tags.ps1
+++ Public/Objects/Tags.ps1
@@ -67,11 +67,52 @@ function New-PATag {
         Method               = 'post'
         Uri                  = "$($ObjectAPIURI)$($ArgumentString)"
         SkipCertificateCheck = $True
-        body                 = $newObject | ConvertTo-Json
+        body                 = $newObject | ConvertTo-Json -Depth 50
     }
     "[$($MyInvocation.MyCommand.Name)] Submitting '$Name' to API endpoint."
+    Write-Debug "[$($MyInvocation.MyCommand.Name)] $($newObject|convertto-json -Depth 50)"
     $Result = Invoke-PaRequest $restParams
     $Result.result
+}
+
+function Set-PATag {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory = $False)][object]$paConnection = $Script:paConnection,
+        [Parameter(Mandatory = $True, Position = 0)][string]$Name,
+        [Parameter(Mandatory = $True, Position = 1)][string]$Comments = ''
+    )
+    #Write-Error "Not Implemented" 
+    #<#
+    $ObjectAPIURI = "$($paConnection.ApiBaseUrl)Objects/Tags?"
+    $Arguments = @{
+        location = 'vsys'
+        vsys     = $($paConnection.VSys)
+        name     = $Name
+    }
+    $Argumentstring = (New-PaArgumentString $Arguments)
+    # Since this is such a simple object with no members, we're just going to update it the same way we would if we were creating a new one.
+    # The only difference is the 'put' http method.
+    # For a more complex example, see the Set-PAAddressGroupStatic cmdlet.
+    [psobject]$newObject = @{
+        entry = @{
+            '@name'     = $Name
+            '@location' = 'vsys'
+            comments    = $Comments
+        }
+    }
+
+    $restParams = @{
+        Method               = 'put'
+        Uri                  = "$($ObjectAPIURI)$($ArgumentString)"
+        SkipCertificateCheck = $True
+        body                 = $newObject | ConvertTo-Json -Depth 50
+    }
+    "[$($MyInvocation.MyCommand.Name)] Submitting '$Name' to API endpoint."
+    Write-Debug "[$($MyInvocation.MyCommand.Name)] $($newObject|convertto-json -Depth 50)"
+    $Result = Invoke-PaRequest $restParams
+    $Result.result
+    #>
 }
 
 Export-ModuleMember -Function "*-*"
